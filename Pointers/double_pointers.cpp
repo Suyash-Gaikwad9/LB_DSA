41e888653121fbe663cc049a64ee9f7f154ff364
suyash <suyash.gaikwad21@vit.edu>
2022-08-13 22:18:33 +0530

    final commit of the day

--- Pointers/double_pointers.cpp
+++ Pointers/double_pointers.cpp
@@ -1,13 +1,24 @@
 #include<iostream>
 using namespace std;
 
+
+void update(int **p2){
+    // p2 = p2 + 1;
+    // //No change
+    // *p2 = *p2 + 1;
+    // //Change
+    **p2 = **p2 + 1;
+    //change
+}
+
+
 int main(){
 
     int i = 5;
     int *p = &i;
     int **p2 = &p;
 
-
+/*
     //printing very first level
     cout<<endl<<"printing very first level "<<endl;
     cout<<i<<endl;
@@ -25,6 +36,18 @@ int main(){
     cout<<endl<<"printing second level "<<endl;
     cout<<&p<<endl;
     cout<<p2<<endl;
+*/
+
+    cout<<endl<<"Before Update..!"<<endl;
+    cout<<"Before "<<i<<endl;
+    cout<<"Before "<<p<<endl;
+    cout<<"Before "<<p2<<endl;
+    update(p2);
+    cout<<endl<<"After Update..!"<<endl;
+    cout<<"After "<<i<<endl;
+    cout<<"After "<<p<<endl;
+    cout<<"After "<<p2<<endl;
+
 
 
 
