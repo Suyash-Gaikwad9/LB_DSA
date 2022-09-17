7e941bedcb6972181d17cdd9b9056250aaa8aa78
Suyash <suyash.gaikwad21@vit.edu>
2022-09-17 17:31:44 +0530

    Heap Sort Implemented

--- Heap/heap_intro.cpp
+++ Heap/heap_intro.cpp
@@ -1,91 +1,143 @@
-#include<iostream>
+#include <iostream>
+#include <queue>
 using namespace std;
 
+class heap
+{
 
-class heap{
-
-    public:
+public:
     int arr[100];
     int size;
 
-    heap(){
+    heap()
+    {
         arr[0] = -1;
         size = 0;
     }
 
-    void insert(int val){
+    void insert(int val)
+    {
         size = size + 1;
         int index = size;
         arr[index] = val;
 
-        while(index > 1){
-            int parent = index/2;
-            if(arr[parent] < arr[index]){
+        while (index > 1)
+        {
+            int parent = index / 2;
+            if (arr[parent] < arr[index])
+            {
                 swap(arr[parent], arr[index]);
                 index = parent;
             }
-            else{
-                return ;
+            else
+            {
+                return;
             }
         }
     }
-    void print(){
-        for(int i = 1; i<=size; i++){
-            cout<<arr[i]<<" ";
+    void print()
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
-    void deletefromHeap(){
-        if(size == 0){
-            cout<<"Nothing to delete..!"<<endl;
-            return ;
+    void deletefromHeap()
+    {
+        if (size == 0)
+        {
+            cout << "Nothing to delete..!" << endl;
+            return;
         }
-        //step1 - put last element into first index
+        // step1 - put last element into first index
         arr[1] = arr[size];
-        //step 2 - remove last element 
+        // step 2 - remove last element
         size--;
 
-        //step 3 : take root node to its correct position
+        // step 3 : take root node to its correct position
         int i = 1;
-        while(i<size){
-            int leftIndex = 2*i;
-            int rightIndex = 2*i+1;
+        while (i < size)
+        {
+            int leftIndex = 2 * i;
+            int rightIndex = 2 * i + 1;
 
-            if(leftIndex < size && arr[i] < arr[leftIndex]){
+            if (leftIndex < size && arr[i] < arr[leftIndex])
+            {
                 swap(arr[i], arr[leftIndex]);
                 i = leftIndex;
             }
-            else if(rightIndex < size && arr[i] < arr[leftIndex]){
+            else if (rightIndex < size && arr[i] < arr[leftIndex])
+            {
                 swap(arr[i], arr[rightIndex]);
                 i = rightIndex;
             }
-            else{
-                return ;
+            else
+            {
+                return;
             }
         }
     }
 };
 
-void heapify(int arr[], int n, int i){
+void heapify(int arr[], int n, int i)
+{
+    // for max heap
     int largest = i;
-    int left = 2*i;
-    int right = 2*i+1;
-    if(left < n && arr[largest]<arr[left]){
+    int left = 2 * i;
+    int right = 2 * i + 1;
+    if (left <= n && arr[largest] < arr[left])
+    {
         largest = left;
     }
-    if(right < n && arr[largest] < arr[right]){
+    if (right <= n && arr[largest] < arr[right])
+    {
         largest = right;
     }
-    if(largest != i){
+    if (largest != i)
+    {
         swap(arr[largest], arr[i]);
-        heapify(arr,n,largest);
+        heapify(arr, n, largest);
     }
 }
 
+void heapSort(int arr[], int n)
+{
+    int size = n;
+    while (size > 1)
+    {
+        // step 1 : swap
+        swap(arr[size], arr[1]);
+        // step 2 : decrement size and call heapify for max heap
+        size--;
+        heapify(arr, size, 1);
+    }
+}
 
+void heapifyMin(int arr[], int n, int i)
+{
+    // for min heap
+    int smallest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+    if (left < n && arr[smallest] > arr[left])
+    {
+        smallest = left;
+    }
+    if (right < n && arr[smallest] > arr[right])
+    {
+        smallest = right;
+    }
+    if (smallest != i)
+    {
+        swap(arr[smallest], arr[i]);
+        heapify(arr, n, smallest);
+    }
+}
 
-int main(){
+int main()
+{
 
     heap h;
     h.insert(50);
@@ -98,13 +150,70 @@ int main(){
     h.print();
     int arr[6] = {-1, 54, 53, 55, 52, 50};
     int n = 5;
-    for(int i = n/2; i>0; i--){
-        heapify(arr,n,i);
+    // heap creation
+    for (int i = n / 2; i > 0; i--)
+    {
+        heapify(arr, n, i);
     }
-    cout<<"Printing array after heapify operation : "<<endl;
-    for(int i=1; i<=n; i++){
-        cout<<arr[i]<<" ";
+    cout << "Printing array after heapify operation : " << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
+
+    // heap sort
+    heapSort(arr, n);
+
+    cout << "Printing sorted array after heap sort operation : " << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Using Priority Queue..." << endl;
+    // priority queue is by default on max heap
+    priority_queue<int> pq;
+
+    pq.push(4);
+    pq.push(2);
+    pq.push(5);
+    pq.push(3);
+
+    cout << "Element at top in priority queue : " << pq.top() << endl;
+    pq.pop();
+    cout << "Element at top in priority queue : " << pq.top() << endl;
+    cout << "Size of priority queue : " << pq.size() << endl;
+
+    if(pq.empty()){
+        cout<<"Queue is empty...!"<<endl;
+    }
+    else{
+        cout<<"Queue is not empty...!"<<endl;
+    }
+
+
+    //min heap using priority queue
+    priority_queue<int, vector<int>, greater<int> >minHeap;
+
+    minHeap.push(4);
+    minHeap.push(2);
+    minHeap.push(5);
+    minHeap.push(3);
+
+    cout << "Element at top in priority queue : " << minHeap.top() << endl;
+    minHeap.pop();
+    cout << "Element at top in priority queue : " << minHeap.top() << endl;
+    cout << "Size of priority queue : " << minHeap.size() << endl;
+
+    if(minHeap.empty()){
+        cout<<"Queue is empty...!"<<endl;
+    }
+    else{
+        cout<<"Queue is not empty...!"<<endl;
+    }
+
+
     return 0;
 }
